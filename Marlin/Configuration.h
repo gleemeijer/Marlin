7b52344f27127a2abfeaa4e407e74f20a4687df8
Gerard <g.leemeijer@gmail.com>
2020-10-31 10:02:47 +0100

    Sheet changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 140 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 141.41 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -825,7 +825,7 @@
  *
  * See https://github.com/synthetos/TinyG/wiki/Jerk-Controlled-Motion-Explained
  */
-//#define S_CURVE_ACCELERATION
+#define S_CURVE_ACCELERATION
 
 //===========================================================================
 //============================= Z Probe Options =============================
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 23, 10, -1.050 }
+#define NOZZLE_TO_PROBE_OFFSET { 23, 10, -1.290 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
